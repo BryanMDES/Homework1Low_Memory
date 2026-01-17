fe55b033882b9730dfffcf2c9e837e05fd8a48fb
BryanMDES <bryan.desantiago14@gmail.com>
2026-01-17 16:48:38 -0600

    Finished half precision

--- homework/half_precision.py
+++ homework/half_precision.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 import torch
+import torch.nn.functional as F
 
-from .bignet import BIGNET_DIM, LayerNorm  # noqa: F401
-
+#from .bignet import BIGNET_DIM, LayerNorm  # noqa: F401
+from .bignet import BigNet, LayerNorm
 
 class HalfLinear(torch.nn.Linear):
     def __init__(
@@ -17,39 +18,45 @@ class HalfLinear(torch.nn.Linear):
         Feel free to use the torch.nn.Linear class as a parent class (it makes load_state_dict easier, names match).
         Feel free to set self.requires_grad_ to False, we will not backpropagate through this layer.
         """
-        # TODO: Implement me
-        raise NotImplementedError()
+        super().__init__(in_features, out_features, bias=bias)
+
+        self.weight.data = self.weight.data.to(torch.float16)
+        if self.bias is not None:
+            self.bias.data = self.bias.data.to(torch.float16)
+
+        self.requires_grad_(False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Hint: Use the .to method to cast a tensor to a different dtype (i.e. torch.float16 or x.dtype)
         # The input and output should be of x.dtype = torch.float32
-        # TODO: Implement me
-        raise NotImplementedError()
+        #TODO
+        x16 = x.to(torch.float16)
+        w = self.weight.detach()
+        b = self.bias.detach() if self.bias is not None else None
+        y16 = F.linear(x16, w, b) 
+        return y16.to(x.dtype) 
+
+def _replace_linears(module: torch.nn.Module) -> None:
+    for name, child in module.named_children():
+        if isinstance(child, torch.nn.Linear) and not isinstance(child, HalfLinear):
+            new = HalfLinear(child.in_features, child.out_features, bias=(child.bias is not None))
+            setattr(module, name, new)
+        else:
+            _replace_linears(child) 
 
 
-class HalfBigNet(torch.nn.Module):
+class HalfBigNet(BigNet):
     """
     A BigNet where all weights are in half precision. Make sure that the normalization uses full
     precision though to avoid numerical instability.
     """
-
-    class Block(torch.nn.Module):
-        def __init__(self, channels: int):
-            super().__init__()
-            # TODO: Implement me (feel free to copy and reuse code from bignet.py)
-            raise NotImplementedError()
-
-        def forward(self, x: torch.Tensor):
-            return self.model(x) + x
-
     def __init__(self):
         super().__init__()
-        # TODO: Implement me (feel free to copy and reuse code from bignet.py)
-        raise NotImplementedError()
+        _replace_linears(self)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
-
+        for m in self.modules():
+            if isinstance(m, LayerNorm):
+                m.to(torch.float32)
 
 def load(path: Path | None) -> HalfBigNet:
     # You should not need to change anything here
